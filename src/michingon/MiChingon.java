e5fa80ee68ec543938e097277c7ee44a96dce4ad
Hp EliteBook <Hp EliteBook@10.168.83.4>
2025-05-19 08:49:53 -0600

    FirstCode

--- src/michingon/MiChingon.java
+++ src/michingon/MiChingon.java
@@ -14,7 +14,8 @@ public class MiChingon {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        // TODO code application logic here\
+        System.out.println("Hola mundo");
     }
     
 }
